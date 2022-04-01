53b2c6b9a5ec0b1f7c343179af100642ce9c00d1
ojihyun0823 <ojihyun0823@gmail.com>
2022-04-01 11:18:59 +0900

    post page staff access

--- blog/tests.py
+++ blog/tests.py
@@ -8,6 +8,9 @@ class TestView(TestCase):
         self.client = Client()
         self.user_trump = User.objects.create_user(username='trump', password='somepassword')
         self.user_obama = User.objects.create_user(username='obama', password='somepassword')
+        self.user_obama.is_staff = True
+        self.user_obama.save()
+
         self.category_asia = Category.objects.create(name='asia', slug='asia')
         self.category_europe = Category.objects.create(name='europe', slug='europe')
 
@@ -169,9 +172,13 @@ class TestView(TestCase):
         response = self.client.get('/blog/create_post/')
         self.assertNotEqual(response.status_code, 200)
 
-        #로그인을 한다.
+        #staff가 아닌 trump가 로그인을 한다.
         self.client.login(username='trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
 
+        #staff가 obama로 로그인힌다.
+        self.client.login(username='obama', password='somepassword')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -190,4 +197,4 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 4)
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
-        self.assertEqual(last_post.author.username, 'trump')
+        self.assertEqual(last_post.author.username, 'obama')
