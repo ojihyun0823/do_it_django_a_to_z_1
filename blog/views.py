9b89ba567d77327754adab02a7792a7d5b1aea3c
ojihyun0823 <ojihyun0823@gmail.com>
2022-04-03 19:39:47 +0900

    delete comment

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.utils.text import slugify
 from django.shortcuts import get_object_or_404
 from .models import Post, Category, Tag, Comment
-from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
+from .forms import CommentForm
 
 
 class PostList(ListView):
@@ -28,7 +28,6 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         context['comment_form'] = CommentForm
         return context
-
 
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -63,7 +62,6 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
-
     template_name = 'blog/post_update_form.html'
 
     def get_context_data(self, **kwargs):
@@ -73,7 +71,6 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
             for t in self.object.tags.all():
                 tags_str_list.append(t.name)
             context['tags_str_default'] = '; '.join(tags_str_list)
-
         return context
 
     def dispatch(self, request, *args, **kwargs):
@@ -85,13 +82,11 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         response = super(PostUpdate, self).form_valid(form)
         self.object.tags.clear()
-
         tags_str = self.request.POST.get('tags_str')
         if tags_str:
             tags_str = tags_str.strip()
             tags_str = tags_str.replace(',', ';')
             tags_list = tags_str.split(';')
-
             for t in tags_list:
                 t = t.strip()
                 tag, is_tag_created = Tag.objects.get_or_create(name=t)
@@ -99,7 +94,6 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
                     tag.slug = slugify(t, allow_unicode=True)
                     tag.save()
                 self.object.tags.add(tag)
-
         return response
 
 
@@ -136,10 +130,10 @@ def tag_page(request, slug):
         }
     )
 
+
 def new_comment(request, pk):
     if request.user.is_authenticated:
         post = get_object_or_404(Post, pk=pk)
-
         if request.method == 'POST':
             comment_form = CommentForm(request.POST)
             if comment_form.is_valid():
@@ -160,30 +154,16 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
-
-            return super(CommentUpdate, self). dispatch(request, *args, **kwargs)
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
 
 
-
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
-#
-#     return render(
-#         request,
-#         'blog/index.html',
-#         {
-#             'posts': posts,
-#         }
-#     )
-# def single_post_page(request, pk):
-#     post = Post.objects.get(pk=pk)
-#
-#     return render(
-#         request,
-#         'blog/single_post_page.html',
-#         {
-#             'post': post,
-#         }
-#     )
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
