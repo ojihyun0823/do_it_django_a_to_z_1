021ffc91f7257c84456d86bbf9ac8c11974f4c09
ojihyun0823 <ojihyun0823@gmail.com>
2022-04-03 12:39:09 +0900

    comment in the post detailed page

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 admin.site.register(Post)
 
@@ -11,3 +11,5 @@ class TagAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
+
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,7 @@ class Category(models.Model):
     def get_absolute_url(self):
         return f'/blog/category/{self.slug}/'
 
+
     # class Meta:
     #     verbose_name_plural = 'categories'
 
@@ -45,3 +46,16 @@ class Post(models.Model):
         return os.path.basename(self.file_upload.name)
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
